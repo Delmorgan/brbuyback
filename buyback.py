7f308c152b08690efa3eaddfee035bf582fc0988
Delmorgan <delmorgan@msn.com>
2024-04-15 21:22:20 -0600

    Update buyback.py

--- buyback.py
+++ buyback.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 base_url = "https://esi.evetech.net/latest"
 
 # Your application's client ID and secret key
-client_id = "YOUR_CLIENT_ID"
-client_secret = "YOUR_CLIENT_SECRET"
+client_id = "133d439d1f864dbe9b7dc8fcc3cea648"
+client_secret = "Ey0z2ijrwchA8wAlaT0ubdQtchbB1CxhmKcodgYK"
 
 # Route to fetch materials in ship hangar
 @app.route('/materials')
